004d3a3e5a6db16c671876222efa2c7ca928e00f
Trent Hashimoto <hashimotot@spu.edu>
2020-02-18 18:04:52 -0800

    Raphtalia stores infractions using sequelize and mySQL

--- bot.js
+++ bot.js
@@ -1,19 +1,48 @@
 const Discord = require('discord.js');
+const Sequelize = require('sequelize');
 const config = require('./config.json');
+const permissions = require('./permissions.json');
+const connection = require('./connection.json');
 const client = new Discord.Client();
+var sequelize = new Sequelize('mysql://'+connection.user+':'+connection.password+'@localhost:3306/raphtalia');
+var infractions = sequelize.import('./sequelize_models/infractions.js');
 const prefix = '!';
 
+console.log('Connected!');
 // When the client is ready, run this code
 // This event will only trigger one time after logging in
 client.once('ready', () => {
+    // Login to Discord with your app's token
     console.log('Ready!');
+    sequelize.sync({ force: true }).then(() => {
+        console.log('Database synced!');
+    })
 });
 
-// Login to Discord with your app's token
-client.login(config.token);
+client.login(config.token).then(() => {
+    console.log('Logged in!');
+});
 
 client.on('message', message => {
-    // Only respond to "!" messages
+    if(message.content.match(/capitalism/gi) != null) {
+        sequelize.transaction(function(t) {
+            return infractions.findOrCreate({
+                where: {
+                    id: message.author.id
+                },
+                transaction: t
+            }).spread(function(user, created) {
+                user.update({
+                    infractionsCount: sequelize.literal('infractionsCount + 1')
+                }).then(() => {
+                    message.channel.send('That\'s a funny way to spell \"Communism\". This infraction has been recorded');
+                    reportInfractions(user.id, message);
+                });
+            })
+        })
+    }
+
+    // Commands
     if(!message.content.startsWith(prefix) || message.author.bot) {
         return;
     }
@@ -25,21 +54,26 @@ client.on('message', message => {
     else if(command === 'kick') {
         // Iterate through every argument and check if it's a mention
         for(var i = 0; i < args.length; i++) {
-            console.log(args[i]);
             const user = getUserFromMention(args[i]);
             if(!user) {
                 continue;
             }
             var member = message.guild.members.get(user.id);
-            console.log(member.displayName);
             member.kick().then((member) => {
-                message.channel.send(":wave: " + member.displayName + " has been kicked");
+                message.channel.send(':wave: ' + member.displayName + ' has been kicked');
             }).catch(() => {
-                message.channel.send("I don't have to listen to you");
+                message.channel.send('I don\'t have to listen to you');
             })
         }
     }
 })
+
+function reportInfractions(userId, message) {
+    infractions.findByPk(userId).then(user => {
+        const discordName = message.guild.members.get(userId).toString();
+        message.channel.send(discordName + ' has incurred ' + user.infractionsCount + ' infractions');
+    });
+}
 
 function getUserFromMention(mention) {
 	// The id is the first and only match found by the RegEx.
@@ -53,7 +87,6 @@ function getUserFromMention(mention) {
 	// However the first element in the matches array will be the entire mention, not just the ID,
 	// so use index 1.
 	const id = matches[1];
-    console.log(matches[1]);
 
 	return client.users.get(id);
 }
