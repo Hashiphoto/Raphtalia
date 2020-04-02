0c004d191080e6aad9058eab6ff880a3e1a5d439
Trent Hashimoto <t.hashiphoto@gmail.com>
2020-04-02 01:11:37 -0700

    Remove special-case banned words

--- censorship.js
+++ censorship.js
@@ -54,12 +54,12 @@ function censorMessage(message) {
     if(!censorshipEnabled) { return }
     
     const sender = message.guild.members.get(message.author.id);
+    
     // The supreme dictator is not censored. Also, immigrants are handled by the Arrive command
     if(helper.hasRole(sender, discordConfig.roles.leader) || helper.hasRole(sender, discordConfig.roles.immigrant)) {
         return false;
     }
     
-    // simple banned words
     if(message.content.match(bannedRegex) != null) { 
         message.delete();
         message.channel.send({embed: {
@@ -70,19 +70,6 @@ function censorMessage(message) {
         }});
 
         helper.addInfractions(sender, message.channel, 1, 'This infraction has been recorded');
-        return true;
-    }
-    // supreme leader disrespect
-    else if(message.content.match(/^(long live|all hail|glory to|hail)/gi) != null &&
-            !message.mentions.roles.find(role => role.name === 'Supreme Dictator') &&
-            message.content.match(/(gulag|supreme leader|leader|erkin|dictator|supreme dictator)/gi) == null) {
-        helper.addInfractions(sender, message.channel, 1, 'Glory to the Supreme Dictator _alone!_ This infraction has been recorded');
-        return true;
-    }
-    // :flag_us:
-    else if(message.content.includes('🇺🇸')) {
-        message.delete();
-        helper.addInfractions(sender, message.channel, 1, 'What flag is that, comrade?');
         return true;
     }
 
