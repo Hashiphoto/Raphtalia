259e0f6de5972875d8fa29a4e57565717b8f959a
hashiphoto <t.hashiphoto@gmail.com>
2020-04-18 22:02:08 -0700

    Update play command to new ytdl-core version

--- youtube.js
+++ youtube.js
@@ -10,15 +10,15 @@ const ytdl = require('ytdl-core');
  * @param {number} vol - The volume to play at (0 to 1)
  */
 function play(voiceChannel, url, vol) {
-    voiceChannel.join()
+    return voiceChannel.join()
     .then(connection => {
         const stream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1<<25  });
         const dispatcher = connection.playStream(stream, { volume: vol, highWaterMark: 1 });
 
         dispatcher.on('end', () => voiceChannel.leave());
     })
-    .catch(e => {
-        console.error(e);
+    .catch(error => {
+        console.log(error);
     })
 }
 
