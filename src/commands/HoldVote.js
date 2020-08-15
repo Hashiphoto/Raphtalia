094842e562174bb2330b127971126147d4d3494e
hashiphoto <t.hashiphoto@gmail.com>
2020-08-14 17:24:30 -0700

    Remove unused imports

--- src/commands/HoldVote.js
+++ src/commands/HoldVote.js
@@ -2,40 +2,11 @@ import Discord from "discord.js";
 import dayjs from "dayjs";
 
 import Command from "./Command.js";
-import links from "../../resources/links.js";
-import db from "../db/db.js";
-import youtube from "../youtube.js";
-import censorship from "../censorship.js";
 import discordConfig from "../../config/discord.config.js";
 import sendTimedMessage from "../util/timedMessage.js";
-import { percentFormat, extractNumber } from "../util/format.js";
-import { clearChannel } from "../util/guildManagement.js";
+import { percentFormat } from "../util/format.js";
 import { dateFormat, parseTime } from "../util/format.js";
-import arrive from "../arrive.js";
-import { softkickMember } from "../util/guildManagement.js";
-import {
-  updateServerStatus,
-  generateServerStatus,
-} from "../util/serverStatus.js";
-import {
-  addInfractions,
-  reportInfractions,
-} from "../util/infractionManagement.js";
-import {
-  addCurrency,
-  getUserIncome,
-  reportCurrency,
-} from "../util/currencyManagement.js";
-import {
-  getNextRole,
-  verifyPermission,
-  pardonMember,
-  exileMember,
-  addRoles,
-  convertToRole,
-  promoteMember,
-  demoteMember,
-} from "../util/roleManagement.js";
+import { convertToRole } from "../util/roleManagement.js";
 import Question from "../Question.js";
 import VotingOption from "../VotingOption.js";
 
