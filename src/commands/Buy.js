9c9ffa7b05d608c016e5c319b0aa0767433f8a77
hashiphoto <t.hashiphoto@gmail.com>
2020-08-20 00:56:00 -0700

    Add BuyController

--- src/commands/Buy.js
+++ src/commands/Buy.js
@@ -10,29 +10,26 @@ class Buy extends Command {
       return this.message.channel.watchSend(`Usage: !Buy (Item Name)`);
     }
     // Get store items
+    // TODO: Move logic into BuyController
     switch (this.message.args[0]) {
       case "promotion":
         let nextRole = getNextRole(this.message.sender, this.message.guild);
         if (!nextRole) {
-          return this.message.channel.watchSend(
-            `You cannot be promoted any higher!`
-          );
+          return this.message.channel.watchSend(`You cannot be promoted any higher!`);
         }
 
         let dbRole = await this.db.roles.getSingle(nextRole.id);
-        this.db.users
-          .get(this.message.sender.id, this.message.guild.id)
-          .then((dbUser) => {
-            if (dbUser.currency < dbRole.price) {
-              return this.message.channel.watchSend(
-                `You cannot afford a promotion. Promotion to ${
-                  nextRole.name
-                } costs $${dbRole.price.toFixed(2)}`
-              );
-            }
-            addCurrency(this.message.sender, -dbRole.price);
-            promoteMember(this.message.channel, null, this.message.sender);
-          });
+        this.db.users.get(this.message.sender.id, this.message.guild.id).then((dbUser) => {
+          if (dbUser.currency < dbRole.price) {
+            return this.message.channel.watchSend(
+              `You cannot afford a promotion. Promotion to ${
+                nextRole.name
+              } costs $${dbRole.price.toFixed(2)}`
+            );
+          }
+          addCurrency(this.message.sender, -dbRole.price);
+          promoteMember(this.message.channel, null, this.message.sender);
+        });
         break;
       default:
         return this.message.channel.watchSend(`Unknown item`);
---
+++ src/controllers/BuyController.js
@@ -0,0 +1,7 @@
+import GuildBasedController from "./GuildBasedController";
+
+import GuildBasedController from "./GuildBasedController";
+
+class BuyController extends GuildBasedController {}
+
+export default BuyController;
