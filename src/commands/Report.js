ffe98b1d10538d95af533c1e3ed9004450d1a09c
hashiphoto <t.hashiphoto@gmail.com>
2020-09-03 17:27:38 -0700

    Report uses controller [RAP-23]

--- src/commands/Report.js
+++ src/commands/Report.js
@@ -1,25 +1,47 @@
 import Command from "./Command.js";
+import MemberController from "../controllers/MemberController.js";
 
 class Report extends Command {
-  execute() {
+  async execute() {
     if (this.message.mentionedMembers.length === 0) {
-      this.message.channel.watchSend("Please repeat the command and specify who is being reported");
-      return;
+      return this.sendHelpMessage();
     }
 
     let amount = 1;
 
-    for (let i = 0; i < this.message.args.length; i++) {
-      let relMatches = this.message.args[i].match(/^\+?\d+$/g);
+    for (const arg of this.message.args) {
+      let relMatches = arg.match(/^\+?\d+$/g);
       if (relMatches) {
         amount = parseInt(relMatches[0]);
         break;
       }
     }
 
-    this.message.mentionedMembers.forEach((target) => {
-      addInfractions(target, this.inputChannel, amount, "Yes sir~!");
-    });
+    const memberController = new MemberController(this.db, this.guild);
+
+    let response = "";
+
+    for (const target of this.message.mentionedMembers) {
+      if (!MemberController.hasAuthorityOver(this.sender, target)) {
+        await memberController
+          .addInfractions(this.sender)
+          .then(
+            (feedback) =>
+              (response += `You must hold a rank higher than ${target} to report them\n${feedback}\n`)
+          );
+        break;
+      }
+
+      await memberController
+        .addInfractions(target, amount)
+        .then((feedback) => (response += feedback));
+    }
+
+    return this.inputChannel.watchSend(response);
+  }
+
+  sendHelpMessage() {
+    return this.inputChannel.watchSend("Usage: `Report @member [+1]");
   }
 }
 
